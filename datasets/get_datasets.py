46ad31f433cf0b2c7ae11a4f87fe9f76774be4c7
Daniel Szemerey <account@szemerey.eu>
2023-03-08 19:03:27 +0100

    feature: Added type annotations.

--- datasets/get_datasets.py
+++ datasets/get_datasets.py
@@ -1,4 +1,7 @@
-def get_all_datasets():
+from typing import Callable, List
+
+
+def get_all_datasets() -> List[Callable]:
     from .m4.preprocess import preprocess as preprocess_m4
 
     return [preprocess_m4]
