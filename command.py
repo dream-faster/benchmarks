310c5fb8e46d8f7cc7a0e578b662edefb29e27cd
Daniel Szemerey <account@szemerey.eu>
2023-03-09 12:22:00 +0100

    fix: Wrong order of arguments.

--- command.py
+++ command.py
@@ -7,15 +7,15 @@ def run():
     parser = argparse.ArgumentParser()
 
     # Add one special argument for deciding what pipeline to run
-    parser.add_argument("--dataset", type=str, default=None)
-    parser.add_argument("--model", type=str, default=None)
+    parser.add_argument("--datasets", type=str, default=None)
+    parser.add_argument("--models", type=str, default=None)
     args = parser.parse_args()
 
-    model_names = args.model.split(",") if args.model is not None else None
+    model_names = args.models.split(",") if args.models is not None else None
 
-    dataset_names = args.dataset.split(",") if args.dataset is not None else None
+    dataset_names = args.datasets.split(",") if args.datasets is not None else None
 
-    main(model_names, dataset_names)
+    main(dataset_names, model_names)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -36,4 +36,4 @@ def main(
 
 
 if __name__ == "__main__":
-    main(dataset_names=["m4"])
+    main(dataset_names=["m4"], model_names=["BaselineNaive"])
