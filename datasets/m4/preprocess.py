e22f668f4f506229651efd3c1ef3701b2853a958
Daniel Szemerey <account@szemerey.eu>
2023-03-09 18:13:39 +0100

    feature(Data): Data downloaded from remote.

--- datasets/m4/preprocess.py
+++ datasets/m4/preprocess.py
@@ -1,15 +1,27 @@
+import os
+
 import pandas as pd
+import wget
+
+
+def download_data() -> None:
+    if not os.path.exists("frontend/data_snippets"):
+        os.makedirs("datasets/m4/original")
+
+    wget.download(
+        "https://github.com/Mcompetitions/M4-methods/raw/master/Dataset/Train/Monthly-train.csv",
+        out="datasets/m4/original",
+    )
 
 
 def preprocess() -> pd.DataFrame:
-    data = pd.read_csv("datasets/m4/Monthly-train.csv")
-    # y = pd.read_csv('./m4/Monthly-test.csv')
+    download_data()
+    data = pd.read_csv("datasets/m4/original/Monthly-train.csv")
 
     data = data.rename(columns={"V1": "m4"})
     data = data.set_index("m4").transpose().reset_index(drop=True)
     data = data.dropna()
 
-    data = data[:50]
     return data
 
 
