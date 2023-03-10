31cc4271c6c6f20b3ee9ab246c26cfbe5340a647
Daniel Szemerey <account@szemerey.eu>
2023-03-10 13:50:02 +0100

    feature: Updated frontend to pull results in.

--- frontend/src/pages/datasets/[id].js
+++ frontend/src/pages/datasets/[id].js
@@ -3,8 +3,10 @@ import { getAllPostIds, getPostData, getResults } from '@/lib/datasets';
 import { Meta } from '@/layouts/Meta.tsx';
 import { Main } from '@/templates/Main.tsx';
 import { getSortedTopicsData } from '@/lib/models';
+import { ProjectPage } from '@/components/ProjectPage';
+import { DataFrame, fromCSV, Series } from 'data-forge';
 
-export default function Post({ postData, filteredTopics }) {
+export default function Post({ postData, results }) {
   return (
     <Main
       wide={true}
@@ -16,13 +18,17 @@ export default function Post({ postData, filteredTopics }) {
         />
       }
     >
-    <article className="prose prose-zinc w-full dark:prose-invert ">
+    <ProjectPage metadata={postData} results={results}/>
+    {/* <article className="prose prose-zinc w-full dark:prose-invert ">
         <h3> {postData.description} </h3>
         <div
           dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
           className="dark:text-slate-100"
         />
       </article>
+      <div>
+        {results}
+      </div> */}
       {/* <ProjectPage
         data={postData}
         relatedData={filteredTopics}
@@ -53,7 +59,11 @@ export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
   // const topicData = await getSortedTopicsData();
   const results = await getResults(params.id)
-
+  
+  const df = fromCSV(results[0]);
+  df.getColumnNames()[0]
+  const result = df.getSeries(df.getColumnNames()[0]).toArray();
+  const date = df.getSeries('Date').toArray();
   // const filteredTopics = topicData.filter((topic) =>
   //   postData.tag.split(',').includes(topic.tag.split(',')[0])
   // );
