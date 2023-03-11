b9f8d7515e03627118dcafbe4843c1d7ebed5c8f
Daniel Szemerey <account@szemerey.eu>
2023-03-11 15:57:03 +0100

    fix: Cleaned index entry.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,4 @@
 // import Plot from 'react-plotly.js';
-import { DataFrame, fromCSV, Series } from 'data-forge';
 import dynamic from 'next/dynamic';
 
 import { Card } from '@/components/card';
@@ -38,7 +37,7 @@ const Table = (props) => {
   );
 };
 
-export default function Index({ date, close }) {
+export default function Index() {
   // const router = useRouter();
 
   return (
@@ -59,46 +58,4 @@ export default function Index({ date, close }) {
       </OneSection>
     </Main>
   );
-}
-
-function fetchCsv() {
-  return fetch(
-    'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/bitcoin.csv'
-  ).then(function (response) {
-    const reader = response.body.getReader();
-    const decoder = new TextDecoder('utf-8');
-
-    return reader.read().then(function (result) {
-      return decoder.decode(result.value);
-    });
-  });
-}
-
-export async function getStaticProps() {
-  // const allPostsData = getSortedPostsData();
-  // const allTopicsData = getSortedTopicsData();
-
-  // const res = await fetch(
-  //   'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/model.csv'
-  // );
-  // const resultsData = await res.json();
-  // const bitcoin = await fetch(
-  //   'https://raw.githubusercontent.com/dream-faster/benchmarking-test/master/results/bitcoin.csv'
-  // );
-  const bitcoinData = await fetchCsv();
-
-  // raw.githubusercontent.com/Unsigned-Research/enoki-research/main/results.csv?token=GHSAT0AAAAAAB65Q4QLH63AHK64XZSFPI2CY77HANA
-
-  // const allTopicsData = allPostsData;
-
-  const df = fromCSV(bitcoinData);
-  const close = df.getSeries('Close').toArray();
-  const date = df.getSeries('Date').toArray();
-
-  return {
-    props: {
-      date,
-      close,
-    },
-  };
 }
