f4d70ad9b0a5348a617b825282aa5fbc10b7c921
Mark Aron Szulyovszky <mark.szulyovszky@gmail.com>
2023-03-12 08:38:27 +0100

    fix: paths added to config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,8 @@ module.exports = withBundleAnalyzer({
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
+  assetPrefix: '/benchmarks/',
+  basePath: '/benchmarks',
   reactStrictMode: false,
   
   typescript:{
